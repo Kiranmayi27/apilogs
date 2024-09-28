cce1ca97d901c2c619627cf8436c669429ff1b39
Naveengoud8184 <naveena81848@gmail.com>
2024-09-28 13:40:16 +0530

    Update operations.js

--- routes/operations.js
+++ routes/operations.js
@@ -2,32 +2,37 @@ const router = require("express").Router();
 const log=require("../utils/logging");
 
 router.get("/",(req,res)=>{
-    log({msg:"ops route"});
+    log(`this is a ops route`);
     res.json({msg:"ops route"});
 });
 
 router.get("/withdraw", (req,res)=>{
-    log({msg:"ops route"});
+    const {id="",name="",status="",msg=""}=req.body;
+    log(`${id} ${name} ${status} withdraw ${msg}`);
     res.json({msg: "withdraw route"});
 });
 
 router.get("/balance_enquiry", (req,res)=>{
-    log({msg:"balance enquiry"});
+    const {id="",name="",status="",msg=""}=req.body;
+    log(`${id} ${name} ${status} balance_enquiry ${msg}`);
     res.json({msg: "balance route"});
 });
 
 router.get("/signup", (req,res)=>{
-    log({msg:"signup route"});
+    const {id="",name="",status="",msg=""}=req.body;
+    log(`${id} ${name} ${status} signup ${msg}`);
     res.json({msg: "signup route"});
 });
 
 router.get("/pin_change", (req,res)=>{
-    log({msg:"pin change"});
+    const {id="",name="",status="",msg=""}=req.body;
+    log(`${id} ${name} ${status} pin_change ${msg}`);
     res.json({msg: "ping change route"});
 });
 
 router.get("/transfer_money", (req,res)=>{
-    log({msg:"transfer money"});
+    const {id="",name="",status="",msg=""}=req.body;
+    log(`${id} ${name} ${status} transfer_money ${msg}`);
     res.json({msg: "transfer money route"});
 });
 

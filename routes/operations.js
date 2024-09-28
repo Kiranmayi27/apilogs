fc75e7a478c66cd98c99c4a1c72455eaace9ed6c
Kiranmayi27 <60614808+Kiranmayi27@users.noreply.github.com>
2024-09-28 15:36:08 +0530

    Update operations.js

--- routes/operations.js
+++ routes/operations.js
@@ -6,31 +6,31 @@ router.get("/",(req,res)=>{
     res.json({msg:"ops route"});
 });
 
-router.get("/withdraw", (req,res)=>{
+router.post("/withdraw", (req,res)=>{
     const {id="",name="",status="",msg=""}=req.body;
     log(`${id} ${name} ${status} withdraw ${msg}`);
     res.json({msg: "withdraw route"});
 });
 
-router.get("/balance_enquiry", (req,res)=>{
+router.post("/balance_enquiry", (req,res)=>{
     const {id="",name="",status="",msg=""}=req.body;
     log(`${id} ${name} ${status} balance_enquiry ${msg}`);
     res.json({msg: "balance route"});
 });
 
-router.get("/signup", (req,res)=>{
+router.post("/signup", (req,res)=>{
     const {id="",name="",status="",msg=""}=req.body;
     log(`${id} ${name} ${status} signup ${msg}`);
     res.json({msg: "signup route"});
 });
 
-router.get("/pin_change", (req,res)=>{
+router.post("/pin_change", (req,res)=>{
     const {id="",name="",status="",msg=""}=req.body;
     log(`${id} ${name} ${status} pin_change ${msg}`);
     res.json({msg: "ping change route"});
 });
 
-router.get("/transfer_money", (req,res)=>{
+router.post("/transfer_money", (req,res)=>{
     const {id="",name="",status="",msg=""}=req.body;
     log(`${id} ${name} ${status} transfer_money ${msg}`);
     res.json({msg: "transfer money route"});
